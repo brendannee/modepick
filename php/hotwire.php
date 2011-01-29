ada91ea4552e55fd67903ce8f0498cd7b5ae5903
Brendan Nee <brendan@blinktag.com>
2011-01-28 21:33:41 -0800

    Updates to curl

--- php/hotwire.php
+++ php/hotwire.php
@@ -2,15 +2,14 @@
 
 include('hotwireAPIkey.php');
 
-$ch = curl_init("http://api.hotwire.com/");
-$fp = fopen("v1/tripstarter/hotel?apikey=".$hotwireAPIkey."&price=*~75&sort=date&limit=1&format=json", "w");
+$ch = curl_init();
+$url = "http://api.hotwire.com/v1/tripstarter/hotel?apikey=".$hotwireAPIkey."&price=*~75&sort=date&limit=1&format=json";
 
-curl_setopt($ch, CURLOPT_FILE, $fp);
+curl_setopt($ch, CURLOPT_URL, $url);
 curl_setopt($ch, CURLOPT_HEADER, 0);
 
 $output = curl_exec($ch);
 curl_close($ch);
-fclose($fp);
 
 echo $output;
 
